da37440f67c8a96c3360e2a07195b3bdbd074d2c
RakibHossen678 <rh491464@gmail.com>
2024-04-25 22:15:39 +0600

    added banner section

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,10 +1,79 @@
-import './Banner.css'
+import "./Banner.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
 const Banner = () => {
-    return (
-        <div>
+  return (
+    <div>
+      <Swiper loop={true} pagination={true} modules={[Pagination]} className="mySwiper">
+        <SwiperSlide className="slide1 rounded-lg bg-cover bg-center">
+        <div className=" flex flex-col justify-center items-center text-white text-center py-52 space-y-4">
+            <h1
+              className="text-5xl font-semibold "
+              
+            >
+              Colors Come Alive: Paint Your World!
+            </h1>
+            <p
+              className="max-w-xl  "
             
-        </div>
-    );
+            >
+              Dive into a world of vibrant colors, endless inspiration, and supportive community. Let your creativity soar as you explore new techniques, share your artwork, and connect with fellow artists.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="slide2 rounded-lg bg-cover bg-center">
+        <div className=" flex flex-col justify-center items-center text-white text-center py-52 space-y-4">
+            <h1
+              className="text-5xl max-w-2xl font-semibold "
+             
+            >
+              Brushes & Pixels: Where Art Meets Technology
+            </h1>
+            <p
+              className="max-w-xl  "
+             
+            >
+               Explore the versatility of different mediums, from watercolors to digital brushes. Connect with artists worldwide, learn from experts, and let your imagination run wild in a supportive and inspiring environment.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="slide3 rounded-lg bg-cover bg-center">
+        <div className=" flex flex-col justify-center items-center text-white text-center py-52 space-y-4">
+            <h1
+              className="text-5xl max-w-2xl font-semibold "
+              
+            >
+              Artistry Unleashed: Paint the Canvas of Your Dreams!
+            </h1>
+            <p
+              className="max-w-xl  "
+              
+            >
+               Join a diverse community of creators, from beginners to professionals, and embark on a journey of self-expression and discovery. With tutorials, challenges, and a supportive community, there's no limit to what you can create.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="slide4 rounded-lg bg-cover bg-center">
+        <div className=" flex flex-col justify-center items-center text-white text-center py-52 space-y-4">
+            <h1
+              className="text-5xl max-w-2xl font-semibold "
+              
+            >
+              Sketch. Paint. Create: Your Artistic Journey Starts Here!
+            </h1>
+            <p
+              className="max-w-xl  "
+            >
+              From pencil sketches to digital paintings, unleash your imagination and express yourself freely. Join a community of like-minded artists, learn new skills, and turn your ideas into beautiful works of art.
+            </p>
+          </div>
+        </SwiperSlide>
+        
+      </Swiper>
+    </div>
+  );
 };
 
 export default Banner;
