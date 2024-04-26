66ba3727f6c82381b832261856d2a4fb7a4cff63
RakibHossen678 <rh491464@gmail.com>
2024-04-26 12:11:21 +0600

    added login functionality

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,10 +1,12 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { createContext, useState } from "react";
 import auth from "../firebase/firebase.config";
 
 export const AuthContext=createContext()
 const AuthProvider = ({children}) => {
     const [user,setUser]=useState(null)
+    const googleProvider = new GoogleAuthProvider();
+    const GithubProvider = new GithubAuthProvider();
 
     const crateUser=(email,password)=>{
         return createUserWithEmailAndPassword(auth,email,password)
@@ -12,11 +14,19 @@ const AuthProvider = ({children}) => {
     const loginUser=(email,password)=>{
         return signInWithEmailAndPassword(auth,email,password)
     }
+    const googleLogin=()=>{
+       return signInWithPopup(auth,googleProvider)
+    }
+    const githubLogin=()=>{
+        return signInWithPopup(auth,GithubProvider)
+    }
 
     const AuthInfo={
         user,
         crateUser,
-        loginUser
+        loginUser,
+        googleLogin,
+        githubLogin
     }
     return (
         <AuthContext.Provider value={AuthInfo}>
